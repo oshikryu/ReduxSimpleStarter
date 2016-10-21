fe11d73122431a06d7da899d199dec7e7176c2dd
Ryuta Oshikiri <ryushikiri@gmail.com>
2016-10-21 16:45:11 -0700

    react tutorial, up to section 1 lesson 15

---
+++ src/components/search-bar.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+// jsx is always transpiled as React.createElement...blahblah
+// so as a result you need to import react
+
+class SearchBar extends Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import SearchBar   from './components/search-bar';
+// make a new component that makes html
+const apiKey = 'AIzaSyD9M488fS-stUtKGYpOLExOfnqMfIxlR8A';
 
-import App from './components/app';
-import reducers from './reducers';
+// take this generated html and put it on the page
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+/*
+ * second arg is the existing html node to render TO
+ * put it in the class="container" div
+ * this is the root node of the entire react application
+ */
+ReactDOM.render(<App />, document.querySelector('.container'));
