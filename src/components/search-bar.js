ca351915d911088dbaa632183157c5f9595c78af
Ryuta Oshikiri <ryushikiri@gmail.com>
2016-10-23 23:32:17 -0700

    finishing section 1

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -4,9 +4,32 @@ import React, { Component } from 'react';
 // so as a result you need to import react
 
 class SearchBar extends Component {
-  render() {
-    return <input />;
+  // called automatically when a new instance is created
+  // used for initializing vars and state
+  constructor(props) {
+    super(props)
+    // only time you will manipulate state as an assigment
+    this.state = {
+      term: '',
+    }
   }
+
+  // adding a value prop makes it a controlled component
+  // value only changes when the state changes
+  // calling set state from onchange handler, the state value has not changed
+  // set state called, component re-renders
+  render() {
+    return (
+      <div>
+        <input value={this.state.term} onChange={evt => this.setState({term: evt.target.value})} />
+        //Value of the input: {this.state.term}
+      </div>
+    );
+  }
+
+  //onInputChange(evt) {
+    //console.log(evt.target.value);
+  //}
 }
 
 export default SearchBar;
